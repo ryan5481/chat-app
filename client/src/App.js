b2e332feea3361657f3a95b247df18e87583e2f6
ryan5481 <ryan5481@me.com>
2023-03-25 23:30:01 +0800

    removed git sub branch from client folder

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import ChatPage from "./components/ChatPage";
+
+import socketIO from "socket.io-client";
+const socket = socketIO.connect(process.env.REACT_APP_API_BASE_URL);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
